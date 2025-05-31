eb187b18056515e47071414881e4630c9d043354
Rosegl4ss <taturyu2@gmail.com>
2025-05-31 13:48:18 +0900

    走るスクリプト

---
+++ Assets/UnityChan/SD_unitychan/Models/run1.cs
@@ -0,0 +1,51 @@
+using System.Diagnostics;
+using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+
+public class caracter : MonoBehaviour
+{
+    Rigidbody rb;
+    private Rigidbody rigidBody;
+    private Animator animator;
+    private float speed = 50f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rigidBody.AddForce(transform.forward * speed, ForceMode.Acceleration);
+            animator.SetBool("Run", true);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rigidBody.AddForce(transform.forward * speed * -1, ForceMode.Acceleration);
+            animator.SetBool("Run", true);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rigidBody.AddForce(transform.right * speed, ForceMode.Acceleration);
+            animator.SetBool("Run", true);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rigidBody.AddForce(transform.right * speed * -1, ForceMode.Acceleration);
+            animator.SetBool("Run", true);
+
+        }
+        if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            animator.SetBool("Run", false);
+        }
+    }
+}
